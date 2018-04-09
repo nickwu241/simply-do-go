80f9a3423539d8b483b9233ca5d31b46dac59060
Nick <nick.wu@shopify.com>
2018-04-08 22:27:43 -0400

    Get rid of detecting PORT since negoroni does it

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 
 	"github.com/nickwu241/simply-do/server"
 )
@@ -14,15 +13,5 @@ func main() {
 		fmt.Printf("error initializing server: %v\n", err)
 		os.Exit(2)
 	}
-	// Use PORT from environment variables if it's set. Needed for Heroku.
-	if portEnv := os.Getenv("PORT"); portEnv != "" {
-		port, err := strconv.Atoi(portEnv)
-		if err != nil {
-			fmt.Printf("error converting PORT environment to integer: %v\n", err)
-			os.Exit(2)
-		}
-		server.Run(fmt.Sprintf(":%d", port))
-	} else {
-		server.Run(":8080")
-	}
+	server.Run()
 }
