638a882af2fec947f9ae3e48c0c245bf78056afa
Nick <nick.wu@shopify.com>
2018-04-17 01:12:01 -0400

    Allow frontend to input all UI changes, do backend async

--- public/main.js
+++ public/main.js
@@ -1,3 +1,74 @@
+var api = {
+    uid: getCookie('x-simply-do-uid', 'default'),
+    getItems: function () {
+        console.debug('GET /api/items', this.uid)
+        return fetch('/api/items', {
+            headers: this._default_headers()
+        }).then(function (response) {
+            return response.json()
+        }).catch(function (e) {
+            console.error(e)
+        })
+    },
+    createItem: function () {
+        console.debug('POST /api/items', this.uid)
+        return fetch('/api/items', {
+            method: 'POST',
+            headers: this._default_headers(),
+            body: JSON.stringify({
+                checked: false,
+                text: ''
+            })
+        }).then(function (response) {
+            return response.json()
+        }).catch(function (e) {
+            console.error(e)
+        })
+    },
+    updateItem: function (item) {
+        console.debug('PUT /api/items', this.uid)
+        return fetch('/api/items/' + item.id, {
+            method: 'PUT',
+            headers: this._default_headers(),
+            body: JSON.stringify(item)
+        }).then(function (response) {
+            return response.json()
+        }).catch(function (e) {
+            console.error(e)
+        })
+    },
+    deleteItem: function (item) {
+        console.debug('DELETE /api/items', this.uid, item)
+        return fetch('/api/items/' + item.id, {
+            method: 'DELETE',
+            headers: this._default_headers()
+        }).then(function (response) {
+            return response.json()
+        }).catch(function (e) {
+            console.error(e)
+        })
+    },
+    _default_headers: function () {
+        return {
+            'x-simply-do-uid': this.uid
+        }
+    },
+}
+
+var workQueue = {
+    addUpdate: function (item) {
+        clearTimeout(this[item.id])
+        this[item.id] = setTimeout(function () {
+            if (item.text !== '') {
+                api.updateItem(item).then(function (item) {
+                    console.debug("UPDATE finished:", item)
+                    delete workQueue[item.id]
+                })
+            }
+        }, 1000)
+    }
+}
+
 var app = new Vue({
     el: '#app',
     data: {
@@ -15,68 +86,38 @@ var app = new Vue({
     watch: {
         uid: function (newValue) {
             setCookie('x-simply-do-uid', newValue, 1)
-            this.getItems()
+            api.uid = newValue
+            api.getItems().then(function (items) {
+                app.items = items
+            })
         }
     },
     methods: {
-        getItems: function () {
-            return fetch('/api/items', {
-                headers: {
-                    'x-simply-do-uid': this.uid
-                }
-            }).then(function (response) {
-                return response.json()
-            }).then(function (body) {
-                console.log(body)
-                app.items = body
-                return body
-            })
-        },
-        createItem: function () {
-            var item = {
+        addNewItem() {
+            var newItem = {
+                id: 'id-placeholder',
                 checked: false,
-                text: ''
+                text: '',
+                time_created: 'time_created-placeholder'
             }
-            return fetch('/api/items', {
-                method: 'POST',
-                headers: {
-                    'x-simply-do-uid': this.uid
-                },
-                body: JSON.stringify(item)
-            }).then(function (response) {
-                return response.json()
-            }).then(function (body) {
-                console.log(body)
-            }).then(function () {
-                app.getItems().then(function () {
-                    app.$refs[app.lastItem.id][0].focus()
-                })
+            this.items.push(newItem)
+            Vue.nextTick().then(function () {
+                app.$refs[app.lastItem.id][0].focus()
+            })
+            api.createItem().then(function (item) {
+                newItem.id = item.id
+                newItem.time_created = item.time_created
+                console.debug("CREATE finished: updated new item:", newItem.id, newItem)
+            }).catch(function (e) {
+                console.error("CREATE errored:", e)
             })
         },
-        updateItem: function (item) {
-            return fetch('/api/items/' + item.id, {
-                method: 'PUT',
-                headers: {
-                    'x-simply-do-uid': this.uid
-                },
-                body: JSON.stringify(item)
-            }).then(function (response) {
-                return response.json()
-            })
-
+        updateDebounced: function (item) {
+            workQueue.addUpdate(item)
         },
         deleteItem: function (item) {
-            fetch('/api/items/' + item.id, {
-                method: 'DELETE',
-                headers: {
-                    'x-simply-do-uid': this.uid
-                }
-            }).then(function (response) {
-                return response.json()
-            }).then(function (body) {
-                app.items = body
-                return body
-            })
+            this._executeAsyncApiDeleteItem(item)
+            this.items.splice(this.items.indexOf(item), 1)
         },
         deleteItemIfEmpty: function (item) {
             if (item.text === '') {
@@ -86,10 +127,24 @@ var app = new Vue({
         uidSync: function () {
             var uid = document.getElementById('uid-input').value
             this.uid = uid ? uid : 'default'
+        },
+        _executeAsyncApiDeleteItem(item) {
+            setTimeout(function () {
+                if (item.id === 'id-placeholder') {
+                    console.debug('DELETE id-placeholder: delaying...')
+                    app._executeApiDeleteItem(item)
+                    return
+                }
+                api.deleteItem(item).then(function () {
+                    console.debug("DELETE finished:", item.id, item)
+                })
+            }, 1000)
         }
     },
     mounted: function () {
-        this.getItems()
+        api.getItems().then(function (items) {
+            app.items = items
+        })
     }
 })
 
