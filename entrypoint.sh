8898079c5ccd150506fe9b4222024e0906f6b2a2
nickwu241 <nick.wu@shopify.com>
2018-06-23 13:54:12 -0400

    Encode Firebase SA base64

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 SECRET_FILE="simply-do-firebase-adminsdk.json"
-echo ${FIREBASE_SERVICE_ACCOUNT_FILE} > $SECRET_FILE
+echo ${FIREBASE_SERVICE_ACCOUNT_FILE_BASE64} | base64 -d > ${SECRET_FILE}
 
 "$@"
